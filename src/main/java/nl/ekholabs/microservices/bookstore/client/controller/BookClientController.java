5eceb3efc415d03d8fea6536ee885cc93abaf2f2
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2016-10-08 11:29:59 +0200

    Add pagination to bookstore-client.

--- src/main/java/nl/ekholabs/microservices/bookstore/client/controller/BookClientController.java
+++ src/main/java/nl/ekholabs/microservices/bookstore/client/controller/BookClientController.java
@@ -9,6 +9,9 @@ import nl.ekholabs.microservices.bookstore.domain.BookDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.PagedResources.PageMetadata;
+import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,38 +30,37 @@ import java.util.stream.Stream;
 @RequestMapping("/books")
 public class BookClientController {
 
-  private final static Logger LOGGER = Logger.getLogger(BookClientController.class.getName());
+    private final static Logger LOGGER = Logger.getLogger(BookClientController.class.getName());
 
-  private BookClientService bookClientService;
+    private BookClientService bookClientService;
 
-  @Autowired
-  public BookClientController(final BookClientService bookClientService) {
-    this.bookClientService = bookClientService;
-  }
+    @Autowired
+    public BookClientController(final BookClientService bookClientService) {
+        this.bookClientService = bookClientService;
+    }
 
-  @HystrixCommand(fallbackMethod = "listBooksFallback")
-  @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public Collection<BookDTO> listBooks() {
-    PagedResources<BookDTO> resources = bookClientService.findAll();
-    Collection<BookDTO> books = resources.getContent();
+    @HystrixCommand(fallbackMethod = "listBooksFallback")
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public PagedResources<BookDTO> listBooks() {
+        return bookClientService.findAll();
+    }
 
-    return books;
-  }
+    @HystrixCommand(fallbackMethod = "createBookFallback")
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Resource<BookDTO> createBook(@RequestBody BookDTO book) {
+        return bookClientService.save(book);
+    }
 
-  @HystrixCommand(fallbackMethod = "createBookFallback")
-  @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
-      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public BookDTO createBook(@RequestBody BookDTO book) {
-    return bookClientService.save(book).getContent();
-  }
+    public PagedResources<BookDTO> listBooksFallback() {
+        LOGGER.log(Level.SEVERE, "The circuit is open for the listBooks() method call.");
+        List<BookDTO> books = Stream.of(BookDTO.createBookDTO("N/A", "N/A", "N/A")).collect(Collectors.toList());
+        PagedResources<BookDTO> booksResource = new PagedResources(books, new PageMetadata(1l, 1l, 1l), new Link("http://localhost:9999"));
+        return booksResource;
+    }
 
-  public Collection<BookDTO> listBooksFallback() {
-    LOGGER.log(Level.SEVERE, "The circuit is open for the listBooks() method call.");
-    return Stream.of(BookDTO.createBookDTO("N/A", "N/A", "N/A")).collect(Collectors.toList());
-  }
-
-  public BookDTO createBookFallback(BookDTO book) {
-    LOGGER.log(Level.SEVERE, "The circuit is open for the createBook() method call.");
-    return BookDTO.createBookDTO("N/A", "N/A", "N/A");
-  }
+    public Resource<BookDTO> createBookFallback(BookDTO book) {
+        LOGGER.log(Level.SEVERE, "The circuit is open for the createBook() method call.");
+        return new Resource(BookDTO.createBookDTO("N/A", "N/A", "N/A"), new Link(""));
+    }
 }
